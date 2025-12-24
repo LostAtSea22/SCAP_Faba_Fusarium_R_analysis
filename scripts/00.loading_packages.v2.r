0336831a1282147c8e512d402850f6e665530e32
Laura Los <Laura.Los@umanitoba.ca>
2025-12-23 20:29:18 -0600

    add variables to script 00

--- scripts/00.loading_packages.v2.r
+++ scripts/00.loading_packages.v2.r
@@ -24,3 +24,20 @@ sapply(c(cran_packages, bioc_packages), require, character.only = TRUE)
 #Save session information
 session_info <- capture.output(sessionInfo())
 writeLines(session_info, here("session_info", paste(Sys.Date(), "session_info.txt", sep = "_")))
+
+# Assign Variables
+path.0.project <- file.path(here()) # home directory, makes use of the package, "here"
+path.1.data <- file.path(path.0.project, "data", "1.data")
+
+path.2.cut <- here("data", "2.cut")
+if(!dir.exists(path.2.cut)) dir.create(path.2.cut)
+names.2.cut.R1 <- file.path(path.2.cut, basename(names.1.data.R1))
+names.2.cut.R2 <- file.path(path.2.cut, basename(names.1.data.R2))
+
+path.3.filter <- here("data", "3.filter")
+if(!dir.exists(path.3.filter)) dir.create(path.3.filter)
+names.3.filter.R1 <- file.path(path.3.filter, basename(names.2.cut.R1))
+names.3.filter.R2 <- file.path(path.3.filter, basename(names.2.cut.R2))
+
+path.4.dada <- here("data", "4.dada")
+if(!dir.exists(path.4.dada)) dir.create(path.4.dada)
