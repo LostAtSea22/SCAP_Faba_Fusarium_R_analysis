c7b95dd52f7b2767a38cc1725467d5a311a9395c
Laura Los <losl@myumanitoba.ca>
2026-01-07 16:27:53 -0600

    adding beepr package to add sound notifications after time-consuming steps
    removed directory creation from set-up script, included in respective analysis script

--- scripts/00.loading_packages.v2.r
+++ scripts/00.loading_packages.v2.r
@@ -3,7 +3,7 @@ classification.ref.ITS <- "D:/reference_data/All_Eukaryotes/UNITE_10.0_sh_genera
 
 #' Load packages
 cran_packages <- c("ggplot2", "stats", "scales", "gridExtra", "hexbin", "knitr", "lme4", 
-                   "lmerTest", "pheatmap", "picante", "plyr", 
+                   "lmerTest", "pheatmap", "picante", "plyr", "beepr",
                    "RColorBrewer", "reshape2", "dplyr", "tidyr", "vegan", "here", "writexl", "readxl")
 inst <- cran_packages %in% installed.packages()
 if(any(!inst)) {
@@ -25,19 +25,8 @@ sapply(c(cran_packages, bioc_packages), require, character.only = TRUE)
 session_info <- capture.output(sessionInfo())
 writeLines(session_info, here("session_info", paste(Sys.Date(), "session_info.txt", sep = "_")))
 
-# Assign Variables
+# Assign Variables for directories
 path.0.project <- file.path(here()) # home directory, makes use of the package, "here"
-path.1.data <- file.path(path.0.project, "data", "1.data")
 
-path.2.cut <- here("data", "2.cut")
-if(!dir.exists(path.2.cut)) dir.create(path.2.cut)
-names.2.cut.R1 <- file.path(path.2.cut, basename(names.1.data.R1))
-names.2.cut.R2 <- file.path(path.2.cut, basename(names.1.data.R2))
-
-path.3.filter <- here("data", "3.filter")
-if(!dir.exists(path.3.filter)) dir.create(path.3.filter)
-names.3.filter.R1 <- file.path(path.3.filter, basename(names.2.cut.R1))
-names.3.filter.R2 <- file.path(path.3.filter, basename(names.2.cut.R2))
-
-path.4.dada <- here("data", "4.dada")
-if(!dir.exists(path.4.dada)) dir.create(path.4.dada)
+# Set u beepr to notify you when Rstudio completes a step
+# notify_when_idle <- function() {while(TRUE) {if (!any(grepl("R", ps -A$output()))) {beep() break} Sys.sleep(5)}}
